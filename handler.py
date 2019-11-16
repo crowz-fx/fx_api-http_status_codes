a91d84510d937227617d30ab4f1d69c4028d7520
Lui Crowie <crowzfx@outlook.com>
2019-11-16 20:44:21 +0000

    finished off models and db lookups

--- handler.py
+++ handler.py
@@ -1,24 +1,26 @@
-STATUS = {
-    "code": 200,
-    "name": "accepted",
-    "description": "jkl;wfjklwjef"
-}
+from flask import abort
+from sqlalchemy import Table
+from server import Session, generate_new_session_connector, marsh, Base
 
-ALL = [
-    {
-        "code": 200,
-        "name": "accepted",
-        "description": "jkl;wfjklwjef"
-    },
-    {
-        "code": 201,
-        "name": "acssdvscepted",
-        "description": "jkl;wsdvsfjklwjef"
-    }
-]
+class StatusCode(Base):
+    __tablename__ = "status_codes"
+    __table_args__ = {'autoload':True }
 
-def return_one_code():
-    return STATUS
+class StatusCodeSchema(marsh.ModelSchema):
+    class Meta:
+        model = StatusCode
+        sqla_session = Session
+
+def return_one_code(code):
+    status = generate_new_session_connector().query(StatusCode).filter(StatusCode.code == code).one_or_none()
+
+    if status is not None:
+        status_schema = StatusCodeSchema()
+        return status_schema.dump(status)
+    else:
+        abort(404, "HTTP Status code for [{status_code}] was not found!".format(status_code = code))
 
 def return_all_codes():
-    return ALL
+    codes = generate_new_session_connector().query(StatusCode).order_by(StatusCode.code).all()
+    status_schema = StatusCodeSchema(many = True)
+    return status_schema.dump(codes)
