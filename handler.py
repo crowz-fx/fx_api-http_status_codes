dd545ade32a0ae779da8d54098adbf58a7297a50
lui <lui@crowz-fx>
2019-11-26 19:14:12 +0000

    added in ability to post to google analytics

--- handler.py
+++ handler.py
@@ -1,10 +1,10 @@
 from flask import abort
 from sqlalchemy import Table
-from server import Session, generate_new_session_connector, marsh, Base
+from server import Session, generate_new_session_connector, marsh, Base, post_to_analytics
 
 class StatusCode(Base):
     __tablename__ = "status_codes"
-    __table_args__ = {'autoload':True }
+    __table_args__ = { "autoload" : True }
 
 class StatusCodeSchema(marsh.ModelSchema):
     class Meta:
@@ -15,12 +15,15 @@ def return_one_code(code):
     status = generate_new_session_connector().query(StatusCode).filter(StatusCode.code == code).one_or_none()
 
     if status is not None:
+        post_to_analytics("/code/{code}".format(code = code))
         status_schema = StatusCodeSchema()
         return status_schema.dump(status)
     else:
+        post_to_analytics("/code/error")
         abort(404, "HTTP Status code for [{status_code}] was not found!".format(status_code = code))
 
 def return_all_codes():
+    post_to_analytics("/all")
     codes = generate_new_session_connector().query(StatusCode).order_by(StatusCode.code).all()
     status_schema = StatusCodeSchema(many = True)
     return status_schema.dump(codes)
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os, sys
 import connexion
+import requests
 from sqlalchemy import create_engine
 from sqlalchemy.pool import SingletonThreadPool
 from sqlalchemy.ext.declarative import declarative_base
@@ -8,11 +9,16 @@ from flask_marshmallow import Marshmallow
 from flask import render_template
 
 debug_through_app = False
-host_ip = '0.0.0.0'
+host_ip = "0.0.0.0"
 port = 5000
+api_version = "v1"
+
+# analytic base info
+ga_version = "1"
+ga_id = ""
 
 if len(sys.argv) > 1 and sys.argv[1] == "--help":
-    print("Usage: --debug-mode --port=<int> --host=\"<string>\"")
+    print("Usage: --debug-mode --port=<int> --host=\"<string>\" --ga-version=<int> --ga-id=\"<sring>\"")
     sys.exit(1)
 
 for arg in sys.argv:
@@ -22,6 +28,10 @@ for arg in sys.argv:
         port = arg.split("=")[1]
     elif arg.startswith("--host"):
         host_ip = arg.split("=")[1]
+    elif arg.startswith("--ga-version"):
+        ga_version = arg.split("=")[1]
+    elif arg.startswith("--ga-id"):
+        ga_id = arg.split("=")[1]
 
 # setup variables for locations
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -30,7 +40,7 @@ sqlalchemy_engine_echo = debug_through_app
 
 # setup the application, create the db connection and marshmallow
 app = connexion.App(__name__, specification_dir = base_dir)
-engine = create_engine(sqlite_url, echo = debug_through_app, connect_args={ 'check_same_thread' : False})
+engine = create_engine(sqlite_url, echo = debug_through_app, connect_args={ "check_same_thread" : False })
 Session = sessionmaker(bind = engine)
 Base = declarative_base(engine)
 marsh = Marshmallow(app.app)
@@ -39,12 +49,23 @@ marsh = Marshmallow(app.app)
 def generate_new_session_connector():
     return Session()
 
+# this is used to post analytics off to google (driven by parameters)
+def post_to_analytics(endpoint):
+    base_url = "https://www.google-analytics.com/collect?"
+    base_url += "v=" + ga_version
+    base_url += "&tid=" + ga_id
+    base_url += "&cid=1" # hardcoded as it comes from this server not client
+    base_url += "&t=pageview" # set option to be pageview, easier collection in analytics
+    base_url += "&dp=%2fapi%2fhttp-status%2f" + api_version + endpoint.replace("/", "%2f")
+    
+    requests.post(base_url)
+
 # main route for the home page
-@app.route('/api/http-status/info')
+@app.route("/api/http-status/info")
 def info():
     return render_template('info.html')
 
 # start up of the server app instance
-if __name__ == '__main__':
-    app.add_api(base_dir + '/swagger.yml')
+if __name__ == "__main__":
+    app.add_api(base_dir + "/swagger.yml")
     app.run(debug = debug_through_app, host = host_ip, port = port)
